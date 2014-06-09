971d407a9306a6bf1c76247368f0305f2022874e
Dave Gill <dave_gill@blueyonder.co.uk>
2014-06-09 21:17:48 +0100

    Added settings and profile form (Empty)

--- FitzosApp.dpr
+++ FitzosApp.dpr
@@ -6,7 +6,19 @@ uses
   untMainForm in 'untMainForm.pas' {frmMain},
   untDataModule in 'untDataModule.pas' {dmdDataModule: TDataModule},
   untBaseForm in 'untBaseForm.pas' {frmBase},
-  untNotifications in 'untNotifications.pas' {frmNotifications};
+  untNotifications in 'untNotifications.pas' {frmNotifications},
+  untFriends in 'untFriends.pas' {frmFriends},
+  untEvents in 'untEvents.pas' {frmEvents},
+  untTeams in 'untTeams.pas' {frmTeams},
+  untNotification in 'untNotification.pas' {frmNotification},
+  untSearch in 'untSearch.pas' {frmSearch},
+  untFriend in 'untFriend.pas' {frmFriend},
+  untEvent in 'untEvent.pas' {frmEvent},
+  untTeam in 'untTeam.pas' {frmTeam},
+  untTeamCreate in 'untTeamCreate.pas' {frmTeamCreate},
+  untEventCreation in 'untEventCreation.pas' {frmEventCreation},
+  untSettings in 'untSettings.pas' {frmSettings},
+  untProfile in 'untProfile.pas' {frmProfile};
 
 {$R *.res}
 
---
+++ untProfile.pas
@@ -0,0 +1,24 @@
+unit untProfile;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TfrmProfile = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmProfile: TfrmProfile;
+
+implementation
+
+{$R *.fmx}
+
+end.
