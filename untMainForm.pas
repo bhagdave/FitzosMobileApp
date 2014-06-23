092af4ee48c7766eda8ab7cb25c3b9f60c2f3aa5
Dave Gill <dave_gill@blueyonder.co.uk>
2014-06-23 16:18:38 +0100

    Only creates one instance of the forms

--- untMainForm.pas
+++ untMainForm.pas
@@ -30,6 +30,7 @@ type
 
 var
   frmMain: TfrmMain;
+  FormRegistry : TStringList;
 
 implementation
 
@@ -40,11 +41,22 @@ uses
 procedure showNewForm(ClassName : String);
 var ObjClass: TFmxObjectClass;
     NewForm: TCustomForm;
+    idx : Integer;
 begin
   ObjClass := TFmxObjectClass(GetClass(ClassName));
   if ObjClass <> nil then
   begin
-    NewForm := ObjClass.Create(Application) as TCustomForm;
+    // is it in the list???
+    idx := FormRegistry.IndexOf(ClassName);
+    if idx > -1 then
+    begin
+      NewForm := FormRegistry.Objects[idx] as TCustomForm;
+    end
+    else
+    begin
+      NewForm := ObjClass.Create(Application) as TCustomForm;
+      FormRegistry.AddObject(ClassName,NewForm);
+    end;
     if Assigned(NewForm) then
       NewForm.Show;
   end
@@ -78,4 +90,10 @@ begin
     showmessage(dmdDataModule.respLogin.Content);
 end;
 
+initialization
+  FormRegistry := TStringList.Create;
+  FormRegistry.Duplicates := dupIgnore;
+  FormRegistry.Sorted := True;
+finalization
+  FreeAndNil(FormRegistry);
 end.
