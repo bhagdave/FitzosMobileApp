703f932088744db03ad54aeb3b9b20cff54dd2af
Dave Gill <dave_gill@blueyonder.co.uk>
2014-06-24 15:09:07 +0100

    Fetching Notifications

--- untNotifications.pas
+++ untNotifications.pas
@@ -5,15 +5,17 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  untBaseForm, FMX.Objects, FMX.Edit, untDataModule, FMX.Layouts;
+  untBaseForm, FMX.Objects, FMX.Edit, untDataModule, FMX.Layouts,
+  FMX.ListView.Types, FMX.ListView, System.Rtti, System.Bindings.Outputs,
+  Fmx.Bind.Editors, Data.Bind.EngExt, Fmx.Bind.DBEngExt, Data.Bind.Components,
+  Data.Bind.DBScope, Fmx.Bind.Grid, Data.Bind.Grid, FMX.Grid;
 
 type
   TfrmNotifications = class(TfrmBase)
-    vertScroller: TVertScrollBox;
-    grdNotifications: TGridPanelLayout;
-    lblNotification: TLabel;
-    btnRead: TButton;
-    btnView: TButton;
+    lvNotifications: TListView;
+    BindSourceDB1: TBindSourceDB;
+    BindingsList1: TBindingsList;
+    LinkListControlToField1: TLinkListControlToField;
     procedure FormShow(Sender: TObject);
   private
     { Private declarations }
@@ -35,6 +37,7 @@ begin
   dmdDataModule.reqNotifications.Params.ParameterByName('id').Value := dmdDataModule.memberId;
   dmdDataModule.reqNotifications.Params.ParameterByName('signature').Value := dmdDataModule.signature('getMemberNotifications');
   dmdDataModule.reqNotifications.Params.ParameterByName('key').Value := dmdDatamodule.getApiKey;
+  dmdDataModule.reqNotifications.Execute;
   dmdDataModule.cdsNotifications.Open;
 end;
 
