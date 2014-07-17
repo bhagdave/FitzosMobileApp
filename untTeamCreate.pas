4ed385b1128103ec4d7f94829e2d0ba167d03905
Dave Gill <dave_gill@blueyonder.co.uk>
2014-07-17 17:02:09 +0100

    Added fields for Team creation

--- untTeamCreate.pas
+++ untTeamCreate.pas
@@ -5,10 +5,17 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  untBaseForm, FMX.Objects, FMX.Edit;
+  untBaseForm, FMX.Objects, FMX.Edit, FMX.ListBox, FMX.Layouts;
 
 type
   TfrmTeamCreate = class(TfrmBase)
+    GridPanelLayout1: TGridPanelLayout;
+    lblName: TLabel;
+    edtName: TEdit;
+    lblSport: TLabel;
+    cboSport: TComboBox;
+    cbActive: TCheckBox;
+    cbPublic: TCheckBox;
   private
     { Private declarations }
   public
