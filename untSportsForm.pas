ede828a9b38f797f71ad00f18a2649ab79abdc01
Dave Gill <dave_gill@blueyonder.co.uk>
2014-08-12 16:44:58 +0100

    Display existing sports in list view

--- untSportsForm.pas
+++ untSportsForm.pas
@@ -5,10 +5,16 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  untBaseForm, FMX.Objects, FMX.Edit;
+  untBaseForm, FMX.Objects, FMX.Edit, FMX.ListView.Types, FMX.ListView, untDataModule,
+  System.Rtti, System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.EngExt,
+  Fmx.Bind.DBEngExt, Data.Bind.Components;
 
 type
   TfrmSports = class(TfrmBase)
+    lvExistingSports: TListView;
+    BindingsList1: TBindingsList;
+    LinkFillControlToField1: TLinkFillControlToField;
+    procedure FormActivate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -18,6 +24,32 @@ type
 implementation
 
 {$R *.fmx}
+uses
+  untMainForm;
+
+procedure TfrmSports.FormActivate(Sender: TObject);
+var
+  sResult : String;
+begin
+  with dmdDataModule do
+  begin
+    // Open up the data.
+    rdsaMemberSports.Active := false;
+    respMemberSports.Content.Empty;
+    reqMemberSports.ClearBody;
+    reqMemberSports.Params.ParameterByName('id').Value := memberId;
+    reqMemberSports.Params.ParameterByName('signature').Value := signature('getSports');
+    reqMemberSports.Params.ParameterByName('key').Value := getApiKey;
+    reqMemberSports.Execute;
+    sResult := getResultString(respMemberSports.Content);
+    if (sResult = 'OK') then
+    begin
+        rdsaMemberSports.Response := respMemberSports;
+        rdsaMemberSports.UpdateDataSet;
+        fdmMemberSports.Open;
+    end;
+  end;
+end;
 
 initialization
 RegisterFMXClasses([TfrmSports]);
