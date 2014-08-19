8c434e10d99ac654a53f1ac24d11a7fc6859881d
Dave Gill <dave_gill@blueyonder.co.uk>
2014-08-19 12:47:33 +0100

    Open up the data on activate

--- untStatsForm.pas
+++ untStatsForm.pas
@@ -59,9 +59,43 @@ begin
 end;
 
 procedure TfrmStats.FormActivate(Sender: TObject);
+var
+  sResult : String;
 begin
   inherited;
   edtDate.Align := TAlignLayout.client;
+  with dmdSports do
+  begin
+    rdsaStatsForSports.Active := false;
+    respStatsForSport.Content.Empty;
+    reqStatsForSport.ClearBody;
+    reqStatsForSport.Params.ParameterByName('id').Value := dmdDataModule.memberId;
+//    reqStatsForSport.Params.ParameterByName('signature').Value := dmdDatamodule.signature('getSports');
+//    reqStatsForSport.Params.ParameterByName('key').Value := dmdDataModule.getApiKey;
+    reqStatsForSport.Execute;
+    sResult := getResultString(respStatsForSport.Content);
+    if (sResult = 'OK') then
+    begin
+        rdsaStatsForSports.Response := respStatsForSport;
+        rdsaStatsForSports.UpdateDataSet;
+        fdmStatsForSport.Open;
+    end;
+
+    rdsaSportsStats.Active := false;
+    respSportsStats.Content.Empty;
+    reqSportsStats.ClearBody;
+    reqSportsStats.Params.ParameterByName('id').Value := dmdDataModule.memberId;
+//    reqSportsStats.Params.ParameterByName('signature').Value := dmdDatamodule.signature('getSports');
+//    reqSportsStats.Params.ParameterByName('key').Value := dmdDataModule.getApiKey;
+    reqSportsStats.Execute;
+    sResult := getResultString(respSportsStats.Content);
+    if (sResult = 'OK') then
+    begin
+        rdsaSportsStats.Response := respSportsStats;
+        rdsaSportsStats.UpdateDataSet;
+        fdmSportsStats.Open;
+    end;
+  end;
 end;
 
 initialization
