107bafa5a981c89ca06259d6b91094ecb75e4ad4
Dave Gill <dave_gill@blueyonder.co.uk>
2014-08-26 17:03:43 +0100

    Fixed profile crashing stuff

--- untProfile.pas
+++ untProfile.pas
@@ -185,11 +185,14 @@ procedure TfrmProfile.FormCreate(Sender: TObject);
 var
     Style: TFMXObject;
 begin
+    {$IFDEF MSWINDOWS}
+        Style := TStyleStreaming.LoadFromResource(HInstance, 'OrangeStyle', RT_RCDATA);
+    {$ENDIF}
     {$IFDEF Android}
-        Style := TStyleManager.LoadFromResource(HInstance, 'OrangeStyle', RT_RCDATA);
+        Style := TStyleStreaming.LoadFromResource(HInstance, 'OrangeStyle', RT_RCDATA);
     {$ENDIF}
     {$IFDEF iOS}
-        Style := TStyleManager.LoadFromResource(HInstance, 'OrangeStyle', RT_RCDATA);
+        Style := TStyleStreaming.LoadFromResource(HInstance, 'OrangeStyle', RT_RCDATA);
     {$ENDIF}
     if Style<> nil then
         TStyleManager.SetStyle(Style);
